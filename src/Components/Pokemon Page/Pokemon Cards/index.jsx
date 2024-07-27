13bf05120ba366c54ac7e750116badc0aaf70438
AmandeepKaur33 <amansandhu8075@gmail.com>
2024-07-27 18:37:38 +0530

    change font size

--- src/Components/Pokemon Page/Pokemon Cards/index.jsx
+++ src/Components/Pokemon Page/Pokemon Cards/index.jsx
@@ -6,7 +6,7 @@ const PokemonCards = ({cardItem}) => {
       <div className='w-full h-full bg-white group-hover:bg-green-500 flex items-center relative justify-center '>
         <img src={cardItem?.image} className='w-44 h-44' alt="" />
         <div className='absolute flex justify-center items-center bg-green-500 group-hover:bg-white w-2/5 h-1/4 rounded-ss-full bottom-0 right-0'>
-      <h1 className='text-xl text-green-100 group-hover:text-green-500'>{cardItem?.name}</h1>
+      <h1 className='text-lg text-green-100 group-hover:text-green-500'>{cardItem?.name}</h1>
         </div>
       </div>
     </div>
