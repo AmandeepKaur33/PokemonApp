8d59e170a269e89436bf8f04216674119827baa2
AmandeepKaur33 <amansandhu8075@gmail.com>
2024-08-23 11:06:22 +0530

    not found functionality

--- src/Components/Pokemon Page/index.jsx
+++ src/Components/Pokemon Page/index.jsx
@@ -4,7 +4,7 @@ import PokemonCards from './Pokemon Cards';
 
 const PokemonApp = () => {
  const [pokemonData, setPokemonData] = useState([]);
- const [fiterData, setFilterData] = useState();
+ const [filterData, setFilterData] = useState();
  const fetchPokemons = async () => {
   const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
   const pokemonData = await Promise.all(
@@ -21,7 +21,15 @@ const PokemonApp = () => {
   useEffect(() => {
     fetchPokemons();
   }, [])
-  console.log(pokemonData);
+  const filter = pokemonData?.filter((element)=> (
+    filterData 
+    ?
+    element?.name?.toLowerCase()?.includes(filterData)
+    :
+    true
+  ));
+console.log(filterData);
+
   return (
     <div className='w-full h-screen flex flex-col items-center  bg-slate-50 py-4'>
          <div className='flex items-center justify-evenly py-5  bg-green-300 rounded-3xl  w-11/12'>
@@ -29,15 +37,14 @@ const PokemonApp = () => {
       </div>
       <div className='w-full flex items-center py-16 justify-center'>
       <div className="flex flex-wrap overflow-auto items-center justify-evenly gap-4 sm:gap-16 drop-shadow-2xl w-11/12 ">
-        {pokemonData?.filter((element)=> (
-          fiterData 
-          ?
-          element?.name?.toLowerCase()?.includes(fiterData)
-          :
-          true
-        )).map((item)=>(
+        {filter.length >0 
+        ?
+        filter.map((item)=>(
           <PokemonCards cardItem={item}/>
-        ))}
+        ))
+      :
+      <h1 className='text-3xl text-red-500 font-semibold'>No result</h1>
+      }
       </div>
       </div>
     </div>
